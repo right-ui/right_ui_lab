0e04ec2e7e293d170abb5129f0e047a4b5f28bb7
c4710n <c4710n@users.noreply.github.com>
2022-04-04 16:11:56 +0800

    feat: preview form

--- lib/lab_web/controllers/preview_controller.ex
+++ lib/lab_web/controllers/preview_controller.ex
@@ -8,9 +8,22 @@ defmodule LabWeb.PreviewController do
   def show(conn, %{"path" => path}) do
     template = "#{Enum.join(path, "/")}.html"
 
+    changeset = create_changeset_for_previewing_form()
+
     conn
     |> assign(:title, Enum.join(path, " / "))
     |> put_layout("preview.html")
-    |> render(template)
+    |> render(template, changeset: changeset)
+  end
+
+  defp create_changeset_for_previewing_form() do
+    data = %{}
+    types = %{name: :string, email: :string, age: :integer}
+    params = %{name: "Charlie Brown", email: "charlie@example.com"}
+
+    {data, types}
+    |> Ecto.Changeset.cast(params, Map.keys(types))
+    |> Ecto.Changeset.validate_required(:age)
+    |> Map.put(:action, :validate)
   end
 end
